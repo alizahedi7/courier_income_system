f9ff1bdaaf4cf02720e6c3b5d22ef97a34253872
alizahedi <alizahedi499@gmail.com>
2024-04-19 23:21:58 +0330

    Add 'Trip' model to 'courier_income' app

--- courier_income/models.py
+++ courier_income/models.py
@@ -1,8 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
+
 
 class Courier(models.Model):
     name = models.CharField(max_length=255)
     
     def __str__(self):
         return self.name
-    
+
+class Trip(models.Model):
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    income = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if self.income < 0:
+            raise ValidationError("Income cannot be negative")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Trip for {self.courier.name} - Income: {self.income}"
