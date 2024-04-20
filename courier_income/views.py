fbc78c8068f8a9eac35111118b8fcb204d244981
alizahedi <alizahedi499@gmail.com>
2024-04-20 06:03:30 +0330

    Implement WeeklyIncome view

--- courier_income/views.py
+++ courier_income/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import WeeklyIncome
+from .serializers import WeeklyIncomeSerializer
 
-# Create your views here.
+class WeeklyIncomeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = WeeklyIncome.objects.all()
+    serializer_class = WeeklyIncomeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['week_start_date']
