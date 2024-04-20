bc0d3decff2d6c74afc8f67e5e84a90087b57d14
alizahedi <alizahedi499@gmail.com>
2024-04-20 04:18:11 +0330

    Add admin panels of existing models

--- courier_income/admin.py
+++ courier_income/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Courier, Trip, TripPenaltyAward, DailyIncome
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TripPenaltyAward)
+class TripPenaltyAwardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DailyIncome)
+class DailyIncomeAdmin(admin.ModelAdmin):
+    pass
+
